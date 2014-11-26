7e652bf611c2b0ee5e69cc5c879ec37af97c48c2
Joe Shaw <joe.r.shaw@gmail.com>
2014-11-26 09:07:57 +0000

    Use nodeunit for tests, add test script to package.json to allow npm test.
    Add basic schema.json tests.

---
+++ test/configSchema.js
@@ -0,0 +1,19 @@
+/*jslint node: true */
+'use strict';
+
+var validate = require('jsonschema').validate;
+var schema = require('../src/schema.json');
+
+function validObject(o) {
+    return !validate(o, schema).errors.length;
+}
+
+exports.validSchema = function(test) {
+
+    test.ok(validObject({ area: 'foo', config: {}}), 'Valid schema, with area.');
+    test.ok(validObject({ area: 'foo', config: { a: 1, b: [1, 2, 3]}}), 'Valid schema, with complex config.');
+    test.ok(!validObject({config: {}}), 'Invalid schema, no area specified.');
+    test.ok(!validObject({ area: 5, config: 'foo', extend: 'x' }), 'Invalid schema, bad data types.');
+
+    test.done();
+};
